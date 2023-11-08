ebaf255203f9f6e1c501298ef8cf6b7c42ce9290
JerryWski <jarek_wasilewski@yahoo.com>
2023-11-08 23:32:53 +0100

    start new project

---
+++ src/components/Navigation/Header.tsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import { useMediaQuery } from '../../utils/useMediaQuery';
+import { motion } from 'framer-motion';
+
+const Header: React.FC = () => {
+  const [toggle, setToggle] = useState(false);
+  const [menuClicked, setMenuClicked] = useState(false);
+  const matches = useMediaQuery('(min-width: 900px)');
+
+  const handleMenuClick = () => {
+    if (matches) {
+      return;
+    }
+    setToggle(false);
+    setMenuClicked(true);
+  };
+
+  // overflow hidden check is toggle is true
+  useEffect(() => {
+    if (toggle) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [toggle]);
+
+  //animatons
+  const container = {
+    hidden: { opacity: 0, y: -30 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        ease: 'linear',
+        staggerChildren: 0.01,
+      },
+    },
+  };
+  const item = {
+    hidden: { opacty: 0, y: -10 },
+    show: { opacty: 1, y: 0 },
+  };
+
+  return (
+    <header>
+      <nav className='nav-container'>
+        <a className='logo-link-desktop' href='/'>
+          <div className='logo-wrapper'>
+            <img src='../../src/assets/base-logo.png' alt='logo-icon' />
+          </div>
+        </a>
+
+        <div className='contact-wrapper'>
+          <a
+            className='contact-icon'
+            href='mailto:office@hinic.eu'
+            aria-label='Link to email'
+          >
+            <i className='fa-solid fa-envelope'></i>
+          </a>
+          <a
+            className='contact-icon'
+            href='tel:+48888433188'
+            aria-label='Link to phone'
+          >
+            <i className='fa-solid fa-phone'></i>
+          </a>
+        </div>
+        {/* check if we are on mobile or not */}
+            <a className='logo-link-mobile' href='/'>
+              <div className='logo-wrapper-mobile'>
+                <img src='../../src/assets/base-logo.png' alt='logo-icon' />
+              </div>
+            </a>
+        {matches && (
+          <div className='nav-links'>
+            <a aria-label='Link to phone' className='link' href='tel:+48888433188'>
+            +48 888 433 188
+            </a>
+            <a className='link' href='/#about'>
+              O Nas
+            </a>
+            <a className='link' href='/prices'>
+              Produkty
+            </a>
+            <a className='link' href='/team'>
+              Zamówienia
+            </a>
+            <a className='link' href='/#gallery'>
+              Kontakt
+            </a>
+          </div>
+        )}
+
+        {!matches && (
+          <div
+            onClick={() => setToggle((prevToggle) => !prevToggle)}
+            className='hamburger'
+          >
+            <motion.span
+              animate={{ rotateZ: toggle ? 45 : 0, y: toggle ? 8 : 0 }}
+              className='burger-line'
+            ></motion.span>
+            <motion.span
+              animate={{ width: toggle ? 0 : 32 }}
+              className='burger-line'
+            ></motion.span>
+            <motion.span
+              animate={{
+                rotateZ: toggle ? -45 : 0,
+                y: toggle ? -8 : 0,
+                width: toggle ? 34 : 28,
+              }}
+              className='burger-line'
+            ></motion.span>
+          </div>
+        )}
+        {toggle && !matches && (
+          <motion.div
+            variants={container}
+            initial='hidden'
+            animate='show'
+            exit={{ opacity: 0 }}
+            className='nav-links-mobile'
+          >
+            <motion.a
+              variants={item}
+              className='link-mobile'
+              href='/#about'
+              onClick={handleMenuClick}
+            >
+              O Nas
+            </motion.a>
+            <motion.a
+              variants={item}
+              className='link-mobile'
+              href='/prices'
+              onClick={handleMenuClick}
+            >
+              Produkty
+            </motion.a>
+            <motion.a
+              variants={item}
+              className='link-mobile'
+              href='/team'
+              onClick={handleMenuClick}
+            >
+              Zamówienia
+            </motion.a>
+            <motion.a
+              variants={item}
+              className='link-mobile'
+              href='/blog'
+              onClick={handleMenuClick}
+            >
+              Kontakt
+            </motion.a>
+          </motion.div>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
