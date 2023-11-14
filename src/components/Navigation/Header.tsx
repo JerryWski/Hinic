712756a1f3a1fc566c6e1279be98f2cb20cec45e
JerryWski <jarek_wasilewski@yahoo.com>
2023-11-14 21:07:18 +0100

    test

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC = () => {
       <nav className='nav-container'>
         <a className='logo-link-desktop' href='/'>
           <div className='logo-wrapper-mobile'>
-            <img src='../../src/assets/base-logo.png' alt='logo-icon' />
+            <img src='./base-logo.png' alt='logo-icon' />
           </div>
         </a>
 
