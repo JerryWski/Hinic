89831041ee24bccc6726a7d2c93a87621374f393
JerryWski <jarek_wasilewski@yahoo.com>
2023-12-12 23:22:02 +0100

    adding new langs

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
     defaultLocale: "pl",
-    locales: ["pl", "en", "fr"],
+    locales: ["pl", "en", "fr", "de", "es"],
     routes: {
       en: {
         o_nas: "about",
@@ -9,6 +9,16 @@ export default {
         kontakt: "contact"
       },
       fr: {
+        o_nas: "about",
+        produkty: "products",
+        kontakt: "contact"
+      },
+      de: {
+        o_nas: "about",
+        produkty: "products",
+        kontakt: "contact"
+      },
+      es: {
         o_nas: "about",
         produkty: "products",
         kontakt: "contact"
