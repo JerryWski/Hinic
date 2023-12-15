c0ac2d7b14d14dae184d14714f2cab684141883b
JerryWski <jarek_wasilewski@yahoo.com>
2023-12-15 21:27:58 +0100

    configuring for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
+import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify(),
-  integrations: [react(),astroI18next()],
+  // output: 'hybrid',
+  // adapter: netlify(),
+  site:  'https://hinic.eu',
+  integrations: [react(),astroI18next(), sitemap()],
   
 });
