b521623d5e89a0ba82f35f032677fe1deda6d7c9
JerryWski <jarek_wasilewski@yahoo.com>
2024-01-20 11:29:43 +0100

    build: output hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'hybrid',
+  output: 'hybrid',
   // adapter: netlify(),
   site:  'https://hinic.eu',
   integrations: [react(),astroI18next(), sitemap()],
