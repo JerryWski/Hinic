e4414cb128e1821e9e21a0d2dbeaed19401580bf
JerryWski <jarek_wasilewski@yahoo.com>
2024-01-20 11:48:00 +0100

    adding node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
 import sitemap from '@astrojs/sitemap';
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'hybrid',
-  // adapter: netlify(),
-  site:  'https://hinic.eu',
-  integrations: [react(),astroI18next(), sitemap()],
-  
+  site: 'https://hinic.eu',
+  integrations: [react(), astroI18next(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
