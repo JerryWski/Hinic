30aa748bf7c86af2565e118c552a8048a51b3f68
JerryWski <jarek_wasilewski@yahoo.com>
2024-01-20 11:49:15 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import react from '@astrojs/react';
 import astroI18next from "astro-i18next";
 import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hinic.eu',
-  integrations: [react(), astroI18next(), sitemap()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  site:  'https://hinic.eu',
+  integrations: [react(),astroI18next(), sitemap()],
+  
 });
