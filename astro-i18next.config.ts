54cfba3c7048735c10a03c067a52e6b540500fe8
JerryWski <jarek_wasilewski@yahoo.com>
2025-05-22 22:51:12 +0200

    adding ru

---
+++ astro-i18next.config.ts
@@ -0,0 +1,32 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "pl",
+    locales: ["en", "fr", "de", "es", "ru"],
+    routes: {
+      en: {
+        "o_nas": "about",
+        "produkty": "products",
+        "kontakt": "contact"
+      },
+      fr: {
+        "o_nas": "about",
+        "produkty": "products",
+        "kontakt": "contact"
+      },
+      de: {
+        "o_nas": "about",
+        "produkty": "products",
+        "kontakt": "contact"
+      },
+      es: {
+        "o_nas": "about",
+        "produkty": "products",
+        "kontakt": "contact"
+      },
+      ru: {
+        "o_nas": "about",
+        "produkty": "products",
+        "kontakt": "contact"
+      }
+    }
+  };
